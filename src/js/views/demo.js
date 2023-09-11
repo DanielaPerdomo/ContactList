dbdce59365dcd3ce61ae0367b933f82175eec55e
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-09-11 03:01:39 +0000

    Estructura modelo del proyecto

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -10,34 +10,29 @@ export const Demo = () => {
 
 	return (
 		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
+			
+			{/* INICIO DEL FORMULARIO DEL CONTACT LIST */}
+			<div className="mb-3">
+				<label htmlFor="exampleFormControlInput1" className="form-label">Full Name</label>
+				<input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Full Name"/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+				<input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter email"/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="exampleFormControlInput1" className="form-label">Phone</label>
+				<input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter phone"/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="exampleFormControlInput1" className="form-label">Address</label>
+				<input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter address"/>
+			</div>
+
 			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
+				<button className="btn btn-primary col-12">Save</button>
 			</Link>
+
 		</div>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="d-flex justify-content-center mt-5">
+
+		{/* INICIO DE LA CARD DE  CONTACTO */}
+
+		<div className="card mb-3" style={{"max-width": "540px"}}>
+  			<div className="row g-0 ">
+				<div className="col-md-4 align-middle">
+					<img src={rigoImage} className="contactImage img-fluid rounded-start" alt="..."/>
+				</div>
+    			<div className="col-md-8 ">
+      				<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text"><i class="fa-solid fa-location-dot fa-fade"></i></p>
+						<p className="card-text"><i class="fa-solid fa-phone fa-fade"></i></p>
+						<p className="card-text"><i class="fa-solid fa-envelope fa-fade"></i></p>
+      				</div>
+    			</div>
+  			</div>
+		</div>
+
+
 	</div>
 );
